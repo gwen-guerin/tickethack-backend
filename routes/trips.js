e034db33c695a87b991af200cfd5e5603b1a9847
Gwen Guérin <gwenael.guerin@protonmail.com>
2022-11-09 12:32:33 +0100

    test

--- routes/trips.js
+++ routes/trips.js
@@ -1,24 +1,48 @@
 const { json } = require('express');
 var express = require('express');
 var router = express.Router();
-var trip = require("../trips.json")
-const moment = require("moment")
+var trip = require('../trips.json');
+const moment = require('moment');
+const Carts = require('../models/cart');
 
 router.post('/', (req, res) => {
+  const wantedDate = new Date(req.body.date);
+  const formatedWantedDate = moment(wantedDate).format('DD-MM-YYYY');
+  const trips = trip.filter(
+    (e) =>
+      e.departure === req.body.departure &&
+      e.arrival === req.body.arrival &&
+      moment(e.date.$date).format('DD-MM-YYYY') === formatedWantedDate
+  );
 
-    const wantedDate = new Date(req.body.date)
-    const formatedWantedDate = moment(wantedDate).format("DD-MM-YYYY")
-    const trips = trip.filter(e => e.departure === req.body.departure && e.arrival === req.body.arrival && moment(e.date.$date).format("DD-MM-YYYY") === formatedWantedDate )
+  if (trips) {
+    res.json({ result: true, trips: trips });
+  } else {
+    res.json({ result: false });
+  }
+});
 
-    if(trips){
-        res.json({result : true, trips : trips})
+router.post('/cart', (req, res) => {
+  const { trip, date, price } = req.body;
+
+  const newCart = new Carts({
+    trip,
+    date,
+    price,
+  });
+  newCart.save().then((cartSaved) => {
+    if (cartSaved) {
+      return res.json({ result: true, cartSaved });
     }
-    else {
-        res.json({result : false})
-    }
+    res.json({ result: false, error: 'Try again' });
+  });
+});
+
+router.get('/cart', (req, res) => {
+    Carts.find().then(carts => {
+        console.log(carts);
+        res.json({carts})
+    })
 })
-
-
-
 
 module.exports = router;
